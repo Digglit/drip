87a337e53704ea95fbe4d53ec476168b2db2ec42
Digglit <Josephmarella97@gmail.com>
2020-06-05 12:52:36 -0400

    added some past orders functionality

---
+++ src/components/admin/toolsPage/subcomponents/viewPreviousOrders/viewPreviousOrders.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import ModuleHeader from '../../../../global/moduleHeader/moduleHeader'
+
+const viewPreviousOrders = (props) => {
+  return (
+    <div className='viewPreviousOrdersContainer'>
+      <ModuleHeader header={'View Older Orders'} closeButton={null} />
+    </div>
+  )
+}
+
+const mapStateToProps = (state) => ({
+  
+})
+
+const mapDispatchToProps = {
+  
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(viewPreviousOrders)
---
+++ src/reducers/pastOrdersDisplay.js
@@ -0,0 +1,12 @@
+let initialState = {
+  open: false
+}
+
+const pastOrdersDisplay = (state = initialState, action) => {
+  switch(action.type) {
+    default:
+      return
+  }
+}
+
+export default pastOrdersDisplay
